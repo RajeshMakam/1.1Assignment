4d55bd9ae05b62d9281538ffa268d2a6a5f314eb
Rajesh <Rajesh@DESKTOP-GAGEKBG>
2017-07-02 22:27:49 +0530

    First Commit

---
+++ src/first/Demo1.java
@@ -0,0 +1,16 @@
+package first;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args){
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter some value");
+		String s2 =  userInput.next();
+		System.out.println(s2);
+	}
+	
+	
+}
